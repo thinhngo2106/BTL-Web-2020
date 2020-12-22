09b4e73d1f054b7f66b6216aefe8b317a395f497
phuongbui2207 <57555872+phuongbui2207@users.noreply.github.com>
2020-12-23 02:57:32 +0700

    zzz

--- Front-end/src/components/header.js
+++ Front-end/src/components/header.js
@@ -37,45 +37,33 @@ function Header(props) {
         return(
             <nav className="navbar navbar-expand-md header">
               <Link to="/">
-              <img className="header__logo" src= {process.env.PUBLIC_URL+ "/image/logo.png"} alt="background-img" width={144} height={81} />
+                <img className="header__logo" src= {process.env.PUBLIC_URL+ "/image/logo.png"} alt="background-img" width={144} height={81} />
               </Link>
-            <div className="header__search"> 
-              <input className="header__searchInput" placeholder="Search.." name="term" type="text" value={query} onChange={(e) => setQuery(e.target.value)}/>
-              <Link to ={`/search?query=${query}`}>
-              <button className="search-button" type="submit" onSubmit={submitAction}>
-              <SearchIcon className="header__searchIcon" />
-              </button>
-              </Link>
-            </div>
-            <div className="header__nav">
-                <Link to = "/orderhistory" style={{textDecoration: 'none'}}>
-                <div className="header__option">
-                  <span className="header__optionLineOne">Kiểm tra</span>
-                  <span className="header__optionLineTwo">Đơn hàng</span>
-                </div>
+
+              <div className="header__search"> 
+                <input className="header__searchInput" placeholder="Search.." name="term" type="text" value={query} onChange={(e) => setQuery(e.target.value)}/>
+                <Link to ={`/search?query=${query}`}>
+                  <button className="search-button" type="submit" onSubmit={submitAction}>
+                    <SearchIcon className="header__searchIcon" />
+                  </button>
                 </Link>
+              </div>
+
+              <div className="header__nav">
+                <Link to = "/orderhistory" style={{textDecoration: 'none'}}>
+                  <div className="header__option">
+                    <span className="header__optionLineOne">Kiểm tra</span>
+                    <span className="header__optionLineTwo">Đơn hàng</span>
+                  </div>
+                </Link>
+
                 <div className="header__option">
                 {
                   userInfo ? (
-                    // <div className ="dropdown">
-                    // <Link to="#" style={{textDecoration: 'none'}}>
-                    // <div className="header__option">
-                    // <span className="header__optionLineOne">Xin chào</span>
-                    // <span className="header__optionLineTwo">{userInfo.Fname} {userInfo.Lname}</span>     
-                    // </div>                 
-                    // </Link>
-                    // <ul className="dropdown-content">
-                    //   <Link to="#signout" onClick={signoutHandler} style={{textDecoration: 'none'}}>
-                    //     <span className="dropdown-signout"> Đăng xuất </span>
-                    //     </Link>
-                    // </ul>
-                    // </div>
-
-                  //test 1  
                   <div>
                     <Dropdown className="dropdown">
                       
-                      <Dropdown.Toggle className="dropdown-basic" >
+                      <Dropdown.Toggle className="dropdown-basic" style={{backgroundColor: 'rgba(244,203,36,09)'}}>
                       
                         <div className="header__option">
                           <span className="header__optionLineOne">Xin chào</span>
@@ -85,32 +73,21 @@ function Header(props) {
                       </Dropdown.Toggle>
 
                       <Dropdown.Menu  className="dropdown-signout"> 
-                        <Dropdown.Item > 
-                          <button onClick={signoutHandler} style={{textDecoration: 'none'}}>
+                        <Dropdown.Item style={{backgroundColor: 'white'}}> 
+                          <button className="signout-button "onClick={signoutHandler} >
                             <span className='dropdown-content'> Đăng xuất </span>
                             </button>
                         </Dropdown.Item>
                       </Dropdown.Menu>
                     </Dropdown>
-                    </div>
+                  </div>
 
-                  // <div>
-                  // <span className="header__optionLineOne">Xin chào</span>
-                  // <br/>
-                  // <DropdownButton id="dropdown-basic-button" title={name}>
-                  //   <Dropdown.Item > <Link to="#signout" onClick={signoutHandler} style={{textDecoration: 'none'}}>
-                  //            <span className='dropdown-content'> Đăng xuất </span>
-                  //            </Link></Dropdown.Item>
-                  //   <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                  //   <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                  // </DropdownButton>
-                  // </div>
                   ) : (
                
                   <Link to ="/signin" style={{textDecoration: 'none'}}>
                     <div className="header__option">
-                    <span className="header__optionLineOne">Hello Thinh</span>
-                    <span className="header__optionLineTwo">Đăng nhập</span>     
+                      <span className="header__optionLineOne">Xin chào</span>
+                      <span className="header__optionLineTwo">Đăng nhập</span>     
                     </div>                 
                   </Link>
                 
